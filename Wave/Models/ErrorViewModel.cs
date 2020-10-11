440a2fe514d3896110244695d562f7639ff7ac3f
Francesco Serianni <seriannif@gmail.com>
2020-10-11 11:40:21 +0200

    Aggiungere i file di progetto.

---
+++ Wave/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wave.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
